1582777f36988645a10f79c97980816f91d1d416
Fukuro <anya.androschuk@gmail.com>
2024-11-29 13:02:29 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,19 @@ export const App = () => {
   const [activeTabId, setActiveTab] = useState(tabs[0].id);
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
 
+  // const handleTest = (id) => {
+  //   setActiveTab(id);
+  //   console.log('hey');
+  // }
+
   return (
     <div className="section">
       <h1 className="title">Selected tab is {activeTab.title}</h1>
 
       <Tabs
         tabs={tabs}
-        selectedTab={activeTab}
-        onSelectedTab={id => setActiveTab(id)}
+        selectedTabId={activeTabId}
+        onTabSelected={id => setActiveTab(id)}
       />
     </div>
   );
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 
-export const Tabs = ({ tabs, selectedTab, onSelectedTab }) => {
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  const selectedTab = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
@@ -14,7 +16,7 @@ export const Tabs = ({ tabs, selectedTab, onSelectedTab }) => {
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => onSelectedTab(tab.id)}
+                onClick={() => selectedTab.id !== tab.id && onTabSelected(tab.id)}
               >
                 {tab.title}
               </a>
